2175200f8b3f6485e6e660b8a9ca667da7910770
Camila <camilavaldez@ufl.edu>
2025-11-12 08:26:01 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -14,14 +14,21 @@ def mul(a,b):
     return a*b
 def div(a,b):
     if a==0:
-        raise ZeroDivisionError("Can't divide by 0")
+        raise ZeroDivisionError
     return b/a
 def log(a,b):
     if b<=0:
-        raise ValueError("Invalid number in log")
+        raise ValueError
     return math.log(b,a)
 def exp(a,b):
     return a**b
 
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
 
+def hypotenuse(a,b):
+    return math.hypot(a,b)
 
